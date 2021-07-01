2a5d8e6cd06b9742e931088d547c35945c435c06
HyoJuns <gywns1720@naver.com>
2021-07-01 16:51:49 +0900

    Js Repair

--- src/scripts/event.js
+++ src/scripts/event.js
@@ -1,92 +1,94 @@
-let section_list = [];
-let colorbg = ['starship','strawberry','pastelgreen'];
-$(function(){
-    
-    let  $getsection = () =>{
-            let result = [];
-            let count = $('section').length;
-            for(let i = 1; i <= count; i++)
-            {
-                result.push($(`#section-${i}`))
-            }
 
-            return result;
-        }
-    ;
-    // Section List
-    section_list = $getsection();
+let colorObject = {
+    colorvalue : ['starship', 'strawberry','pastelgreen'],
+    colorname : 'bg-'
+};
 
-    // // console.log(list);
-    // let myanimation = new TimelineLite();
-    // TweenLite.set($container, {visibility : false});
+let sectionObject = {
+    sectionname : 'section-',
+    sectionnumber : [1,2,3]
+}
 
-    // myanimation.from($header, 0.5, {backgroundColor: '#222'});
-
-    setTimeout(Init, .5);
-})
+let prevobject = [];
 
 // 초기화
-function Init()
-{
-
-    TweenLite.fromTo(section_list[0], 1.5, {display:'block', opacity : 0}, {opacity : 1});
-
-    
-    
-    for(let i = 0 ; i < 3; i++)
-    {
-        section_list[i].addClass(`bg-${colorbg[i]}`);
-    }
-    
-    $('#header').addClass(`bg-${colorbg[0]}`);
-    Move();
-}
-
-// Object 움직임
-function Move()
-{
-    let t1 = new TimelineLite();
-
-    let leftbox = $('.leftbox').find('img');
-    let rightbox = $('rightbox');
-    let title = rightbox.find('.title');
-    let description = rightbox.find('.description');
-    let activebutton = rightbox.find('.activebutton');
-
-    t1.fromTo(leftbox, .5, {left : 0},{left:300});
-    t1.from(leftbox, 0.9, {opacity : .1});
-    t1.from(title, 0.2,{opacity : .1, right : 100}, "-=.8");
-    t1.from(description, 0.2,{opacity : .1}, "-=.7");
-    t1.from(activebutton, 0.2, {opacity : 0.1}, "-=.6");
-    t1.play();
-}
-
-
+$(()=>{Init()});
 
 
 // 버튼 이벤트
 $(function(){
-    let fnb = $('#fnb');
 
-    let hyperlink = fnb.find('a');
-
-    hyperlink.on('click',function(e){
-        console.log(e.target.alt);
-        TweenLite.fromTo(section_list[0], 1.5, {display:'block', opacity : 0}, {opacity : 1});
-        $('section').removeClass('active-screen');
-
-        let lastch = e.target.alt.charAt(e.target.alt.length - 1);
-        console.log(lastch);
-        $(`#${e.target.alt}`).addClass('active-screen');
-
-        for(let i = 0 ; i < 3; i++)
-        {
-            TweenLite.fromTo($(`#section-${i+1}`), 1.5, { opacity : 1}, {opacity : 0});
-            TweenLite.fromTo($(`#section-${lastch}`), 1.5, { opacity : 0,display:'block'}, {opacity : 1});
-            $('#header').removeClass(`bg-${colorbg[i]}`);
-        }
-        console.log(colorbg[lastch - 1]);
-        $('#header').addClass(`bg-${colorbg[lastch - 1]}`);
+    $('#fnb').find('img').on('click',eventButton);    
+});
+// 초기화
+function Init()
+{
+    let t1 = new TimelineLite;
+    $('#section-1').addClass(`${colorObject.colorname}${colorObject.colorvalue[0]}`).addClass('active-screen');
+    $('#header').addClass(`${colorObject.colorname}${colorObject.colorvalue[0]}`).addClass('active-screen');;
+    let leftbox = $('#section-1').find('.leftbox');
+    t1.to(leftbox, 1, {
+        x : 0,
+        opacity : 1
     });
-})
 
+    let rightbox = $('#section-1').find('.rightbox');
+    t1.to(rightbox, 1, {
+        x : 0,
+        opacity : 1
+    });
+
+    prevobject[0] = $('#section-1');
+    prevobject[1] = leftbox;
+    prevobject[2] = rightbox;
+}
+
+// 버튼 이벤트
+function eventButton()
+{
+    let imagealt = $(this).attr('alt');
+    
+    let t1 = new TimelineLite;
+    let pret1 = new TimelineLite;
+
+    let index = imagealt.charAt(imagealt.length - 1);
+    $(`section`).removeAttr('class');
+    $("#header").removeAttr('class')
+    let thissection = $(`#${sectionObject.sectionname}${index}`);
+    pret1.to(prevobject[1], .4,{
+        opacity : 0,
+        x : -100
+    });
+    pret1.to(prevobject[2], .4,{
+        opacity : 0,
+        x : 500
+    });
+
+
+    // Class 추가
+    thissection.addClass(`${colorObject.colorname}${colorObject.colorvalue[index - 1]}`).addClass('active-screen');
+    $('#header').addClass(`${colorObject.colorname}${colorObject.colorvalue[index - 1]}`).addClass('active-screen');;
+
+
+
+
+    let leftbox = $(thissection).find('.leftbox');
+    t1.to(leftbox, .5, {
+        x : 0,
+        opacity : 1
+    });
+
+    let rightbox = $(thissection).find('.rightbox');
+    t1.to(rightbox, .5, {
+        x : 0,
+        opacity : 1
+    });
+    
+
+    prevobject[0] = thissection;
+    prevobject[1] = leftbox;
+    prevobject[2] = rightbox;
+    // console.log(`${colorObject.colorname}${colorObject.colorvalue[1]}`);
+
+    t1.restart();
+}
